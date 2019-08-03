343fdc1722e76a8e6642e711df781963d05791f8
MatthewSKC <47339899+MatthewSKC@users.noreply.github.com>
2019-08-04 10:25:44 +1200

    Update
    On Track with Master

--- NCEA_L2_2.7.ino
+++ NCEA_L2_2.7.ino
@@ -1,30 +1,52 @@
 #include <Wire.h>                 // Wire.h is a first party libary
 #include <LiquidCrystal_I2C.h>    // LiquidCrystal_I2C.h is a third party libary for LCD over serial communication
+#include <dht.h>                  // dht.h is for the humidity temperature module (Third Party)
+#include "RTClib.h"               // RTCLib.h is a third party libary for getting the time
 
-#define key1 22
-#define key2 28
-#define key3 24
-#define key4 26
+#define key1 22   // Key 1 (Membrane Keyboard)
+#define key2 28   // Key 2 (Membrane Keyboard)
+#define key3 24   // Key 3 (Membrane Keyboard)
+#define key4 26   // Key 4 (Membrane Keyboard)
+
+#define DHT11_PIN 2
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);    //I2C pins declaration
 
-#include <dht.h>
-dht DHT;
-#define DHT11_PIN 2
+dht DHT;    // start DHT Module
+
+RTC_DS1307 rtc;   // start rtc module
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};   //RTC Days of the week
 
 void setup(){
 
   lcd.begin(16,2); //Defining 16 columns and 2 rows of lcd display
   lcd.backlight(); //To Power ON the back light
   
-  Serial.begin(9600);
-
+  while (!Serial);
+  
+  Serial.begin(9600);   // Start Serial on 9600 Baud    - Temperature Module + Membrane Keyboard
+  Serial.begin(57600);  // Start Serial on 57600 Baud   - RTC Module saying date and time
+  
   pinMode(key1, INPUT_PULLUP); // setting pins to only take input
   pinMode(key2, INPUT_PULLUP);
   pinMode(key3, INPUT_PULLUP);
   pinMode(key4, INPUT_PULLUP);
 
   lcd.clear(); //clearing the LCD screen
+
+      if (! rtc.begin()) {      // Checking for RTC Module
+        Serial.println("Couldn't find RTC");
+        while (1);
+      }
+    
+      if (! rtc.isrunning()) {   // If RTC Module is NOT running set the time, (Battery May need to be replaced)
+        Serial.println("RTC is NOT running!");
+        // following line sets the RTC to the date & time this sketch was compiled
+        // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        // This line sets the RTC with an explicit date & time, for example to set
+        // August 8, 2019 at 9am you would call:    - Delete the // under me once the time is set
+        // rtc.adjust(DateTime(2019, 8, 21, 9, 0, 0));
+      }
 
 }
 void loop () {
@@ -39,11 +61,35 @@ void loop () {
       if(!key1S) {
 
         Serial.println("Key 1 has been pressed");
+        
         lcd.clear();
+        
+        DateTime now = rtc.now();   // Asks RTC for the time
 
         delay(100);
 
         lcd.print("Time");
+        
+        delay(1);
+        
+        lcd.setCursor(0,1);             // LCD Print Time
+        lcd.print(now.hour(), DEC);
+        lcd.print("h ");
+        lcd.print(now.minute(), DEC);
+        lcd.print("m ");
+        lcd.print(now.second(), DEC);
+        lcd.print("s");
+
+        Serial.print(now.hour(), DEC);  // Serial Print time (57600)
+        Serial.print(':');
+        Serial.print(now.minute(), DEC);
+        Serial.print(':');
+        Serial.print(now.second(), DEC);
+        Serial.println();
+
+        delay(3000);
+
+        lcd.clear();
 
       }
       if(!key2S) {
@@ -60,11 +106,33 @@ void loop () {
 
       if(!key3S) {
         
+        lcd.clear();            //Loading animation beacause why not
+        lcd.print("Loading");   // refer: https://github.com/MatthewSKC/arduino2.7int/commit/ad28c4e75fe6ab158ba2342b5c18635478b1f083
+        delay(300);
         lcd.clear();
-        lcd.print("Loading...");
+        lcd.print("lOading");
+        delay(300);
+        lcd.clear();
+        lcd.print("loAding");
+        delay(300);
+        lcd.clear();
+        lcd.print("loaDing");
+        delay(300);
+        lcd.clear();
+        lcd.print("loadIng");
+        delay(300);
+        lcd.clear();
+        lcd.print("loadiNg");
+        delay(300);
+        lcd.clear();
+        lcd.print("loadinG");
+        delay(300);
+        lcd.clear();
+        lcd.print("LOADED!");
         
-        delay(1500);    // refer: https://github.com/MatthewSKC/arduino2.7int/commit/ad28c4e75fe6ab158ba2342b5c18635478b1f083
-
+        
+        delay(500);    
+        
         int chk = DHT.read11(DHT11_PIN);    //takes measurements from temperature sensor
 
         delay(10);
@@ -83,13 +151,17 @@ void loop () {
         lcd.setCursor(0,1);
         delay(10);
         lcd.print(DHT.temperature);
+
+        delay(3000);
+
+        lcd.clear();
         
       }
 
       if(!key4S) {
 
         Serial.println("Key 4 has been pressed");
-            lcd.clear();
+        lcd.clear();
 
 
         delay(100);
@@ -99,5 +171,6 @@ void loop () {
       }
   
   delay(100);
+
 
 }
